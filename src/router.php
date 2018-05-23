298d7fd819532867a142c62cbc29c171c4a60031
0x6368656174 <0x6368656174@gmail.com>
2018-05-23 23:00:27 +1000

    Роутер

--- src/router.php
+++ src/router.php
@@ -1,39 +1,45 @@
 <?php
-if (substr($_SERVER["REQUEST_URI"], 0,32) == '/wp-content/themes/project-theme') {
-    // Для вывода данных в консоль
-    $stderr = fopen('php://stderr', 'w');
-    $url = $_SERVER['REQUEST_URI'];
-    $path = parse_url($url, PHP_URL_PATH);
-    $query = parse_url($url, PHP_URL_QUERY);
-    $newPath = ltrim($path, '/');
-    if ($query) {
-        $newPath .= '?' . $query;
-    }
 
-    $base = 'http://localhost:4201/';
-    $proxyUrl = $base . $newPath;
+// Если запрашиваем данные из темы, то вернем их из WebPack Dev Server
+if (substr($_SERVER["REQUEST_URI"], 0,32) == '/wp-content/themes/%THEME%') {
+  $url = $_SERVER['REQUEST_URI'];
 
-// $context = stream_context_create();
-    $contents = @file_get_contents($proxyUrl /* , false, $context */);
-// Получаем заголовки ответа из глобальной переменной (PHP!)
-    $headers = $http_response_header;
+  $path = parse_url($url, PHP_URL_PATH);
+  $query = parse_url($url, PHP_URL_QUERY);
+
+  $newPath = ltrim($path, '/');
+  if ($query) {
+    $newPath .= '?' . $query;
+  }
+
+  $base = 'http://%HOST%:%UPDATE_PORT%/';
+  $proxyUrl = $base . $newPath;
+
+  // Получим контент из WebPack Dev Server
+  $contents = @file_get_contents($proxyUrl /* , false, $context */);
+
+  // Получаем заголовки ответа из глобальной переменной (PHP)
+  $headers = $http_response_header;
+
+  if ($contents === false) {
     $firstLine = $headers[0];
-    if ($contents === false) {
-        fwrite($stderr, "Request failed: $proxyUrl - $firstLine\n");
-        header("HTTP/1.1 503 Proxy error");
-        die("Proxy failed to get contents at $proxyUrl");
+    $stderr = fopen('php://stderr', 'w');
+    fwrite($stderr, "Request failed: $proxyUrl - $firstLine\n");
+    header("HTTP/1.1 503 Proxy error");
+    die("Proxy failed to get contents at $proxyUrl");
+  }
+
+  // Прокидываем разрешенные заголовки
+  $allowedHeaders = "!^(http/1.1|server:|content-type:|last-modified|access-control-allow-origin|Content-Length:|Etag:|Accept-Ranges:|Date:|Via:|Connection:|X-|age|cache-control|vary)!i";
+  foreach ($headers as $header) {
+    if (preg_match($allowedHeaders, $header)) {
+      header($header);
     }
-//    fwrite($stderr, "$proxyUrl - OK: $firstLine\n");
-    $allowedHeaders = "!^(http/1.1|server:|content-type:|last-modified|access-control-allow-origin|Content-Length:|Etag:|Accept-Ranges:|Date:|Via:|Connection:|X-|age|cache-control|vary)!i";
-// Прокидываем разрешенные заголовки
-    foreach ($headers as $header) {
-        if (preg_match($allowedHeaders, $header)) {
-            header($header);
-        }
-    }
-    header('From-Webpack: true');
-    echo $contents;
-    return true;
+  }
+  header('From-Webpack: true');
+
+  echo $contents;
+  return true;
 }
 
 return false;
--- src/serve.command.ts
+++ src/serve.command.ts
@@ -1,4 +1,6 @@
 import { spawn } from 'child_process';
+import { openSync, readFileSync, writeFileSync } from 'fs';
+import { tmpdir } from 'os';
 import { join } from 'path';
 import * as webpack from 'webpack';
 import * as WebpackDevServer from 'webpack-dev-server';
@@ -47,6 +49,26 @@ interface IArgv {
   theme?: string;
 }
 
+function getRandomInt(min: number, max: number) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+interface ICreateRouterParams {
+  theme: string;
+  host: string;
+  updatePort: number;
+}
+
+function createRouterPhp(params: ICreateRouterParams): string {
+  let routerData = readFileSync(join(__dirname, 'router.php'), 'utf-8');
+  routerData = routerData.replace('%THEME%', params.theme);
+  routerData = routerData.replace('%HOST%', params.host);
+  routerData = routerData.replace('%UPDATE_PORT%', params.updatePort.toString());
+  const filePath = join(tmpdir(), `wp-build-router-${getRandomInt(1000, 9999)}.php`);
+  writeFileSync(filePath, routerData);
+  return filePath;
+}
+
 export function handler(argv: IArgv) {
   process.stdout.write(`Starting serve on http://${argv.host}:${argv.port}...`);
   const mode = argv.prod ? 'production' : 'development';
@@ -54,9 +76,9 @@ export function handler(argv: IArgv) {
 
   const config = readConfig();
   const dist = join(process.cwd(), config.build.outputPath);
-  const router = join(__dirname, 'router.php');
-  const themeName = config.defaultTheme;
-  const publicPath = `/wp-content/themes/${themeName}/`;
+  const themeName = theme || config.defaultTheme;
+
+  const router = createRouterPhp({theme: themeName, host: argv.host, updatePort: argv.updatePort});
 
   const php = spawn('php', ['-S', `${argv.host}:${argv.port}`, '-t', dist, router], {cwd: process.cwd(), env: process.env});
   php.stdout.on('data', data => process.stdout.write(`PHP: ${data}`));
