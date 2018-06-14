4bd89c3449d5cc1f3b182fb7f4cef327c8ef83cb
0x6368656174 <0x6368656174@gmail.com>
2018-06-14 16:38:33 +1000

    add default action

--- src/index.ts
+++ src/index.ts
@@ -6,12 +6,17 @@ import * as buildCommand from './build.command';
 import * as initCommand from './init.command';
 import * as serveCommand from './serve.command';
 
-yargs.usage('Usage: $0 [command]')
+const argv = yargs.usage('Usage: $0 [command]')
   .command(buildCommand)
   .command(serveCommand)
   .command(initCommand)
   .version('1.0.0')
   .locale('en')
   .help('help')
-  .epilog('(c) 2018 Pavel Puchkov <0x6368656174@gmail.com> https://github.com/0x6368656174/wp-build')
+  .epilog('(c) 2018 Pavel Puchkov <0x6368656174@gmail.com> https://github.com/0x6368656174/wp-builder')
   .argv;
+
+if (argv._.length === 0) {
+  yargs.showHelp();
+  process.exit(0);
+}
