949cafc6db8bb3d5eef0e712418f5d6a37a1a950
0x6368656174 <0x6368656174@gmail.com>
2018-06-21 16:12:33 +1000

    Update starter function.php

--- src/starter/src/%PROJECT_THEME_NAME%/functions.php
+++ src/starter/src/%PROJECT_THEME_NAME%/functions.php
@@ -1,13 +1,17 @@
 <?php
 
-require_once( __DIR__ . '/../../../vendor/autoload.php' );
-$timber = new Timber\Timber();
+declare(strict_types=1);
+
+require_once __DIR__.'/../../../vendor/autoload.php';
+
+use Timber\Timber;
+
+$timber = new Timber();
 
 /**
- * Добавим все файлы из functions.php.d
+ * Добавим все файлы из functions.php.d.
  */
-$files = glob(get_template_directory() . "/functions.php.d/*.php");
-foreach ($files as $filename)
-{
-  require_once($filename);
+$files = \glob(get_template_directory().'/functions.php.d/*.php');
+foreach ($files as $filename) {
+  require_once $filename;
 }
