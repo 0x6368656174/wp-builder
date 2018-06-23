55355b7eea314db27688e0a0bb46deb7d6228a97
0x6368656174 <0x6368656174@gmail.com>
2018-06-23 20:40:36 +1000

    fix init command

--- src/commands/init.command.ts
+++ src/commands/init.command.ts
@@ -1,28 +1,8 @@
-import { createInterface } from 'readline';
 import { Options } from 'yargs';
 
 export const command = 'init';
 export const describe = 'Init new project';
 export const builder: {[key: string]: Options} = { };
-
-const rl = createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-async function question(query: string, defaultValue?: string): Promise<string> {
-  const result = await new Promise(resolve => {
-    rl.question(query, answer => resolve(answer));
-  });
-
-  if (result) {
-    return result as string;
-  } else if (defaultValue !== undefined) {
-    return defaultValue;
-  } else {
-    return question(query);
-  }
-}
 
 export async function handler(argv: any) {
   const run = require('./init.command-handler');
