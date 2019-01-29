5251732d85513032aad6fe86e67e8597eb15bd02
Pavel Puchkov <0x6368656174@gmail.com>
2019-01-29 11:15:03 +1000

    fix: Update php-cs-fixer url

--- src/install/php-cs-fixer.ts
+++ src/install/php-cs-fixer.ts
@@ -4,7 +4,7 @@ import * as simpleGet from 'simple-get';
 
 const output = createWriteStream(join(__dirname, '..', 'commands', 'php-cs-fixer'));
 
-(simpleGet as any)('https://cs.sensiolabs.org/download/php-cs-fixer-v2.phar', (err: any, response: any) => {
+(simpleGet as any)('https://cs.symfony.com/download/php-cs-fixer-v2.phar', (err: any, response: any) => {
   if (err) {
     throw err;
   }
